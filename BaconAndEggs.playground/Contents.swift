e9c36caf5f119eae262fefa262fda5ddd00b96b8
rsgc-noblecurveira-c <student@MacBook-Pro-Noble-Curviera.local>
2015-11-18 18:13:39 -0500

    completed the assignments

---
+++ BaconAndEggs.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var baconAge = 6
+
+var eggsAge = 12
+
+if baconAge > 7
+{
+    print("throw out bacon")
+}
+
+if eggsAge > 21
+{
+    print("throw out eggs")
+}
+
+if baconAge <= 7 && eggsAge <= 21
+{
+    print("you can cook the bacon and eggs")
+}
